25552ee41b13afff125bca23dc1d63a2d7a33e0d
Ubuntu <hortinstein@gmail.com>
2016-01-28 22:08:19 -0500

    almost done with shell exploit

--- project1/overflowable.c
+++ project1/overflowable.c
@@ -2,8 +2,12 @@
 #include <stdio.h>
 #include <strings.h>
 
-int main(int argc, char *argv[]){
+void bad_function(){
 	char buffer[12];
 	printf("please enter your name\n");
 	gets(buffer);
+}
+
+int main(int argc, char *argv[]){
+	bad_function();
 };
