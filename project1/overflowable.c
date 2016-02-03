0a03ce25514d33777da3c228b6999e127c05268e
Ubuntu <hortinstein@gmail.com>
2016-02-03 00:05:15 -0500

    changed code, added pictures

--- project1/overflowable.c
+++ project1/overflowable.c
@@ -1,13 +1,7 @@
-
 #include <stdio.h>
 #include <strings.h>
 
-void bad_function(){
-	char buffer[12];
-	printf("please enter your name\n");
-	gets(buffer);
-}
-
 int main(int argc, char *argv[]){
-	bad_function();
+    char buffer[16];
+    strcpy(buffer,argv[1]);
 };
